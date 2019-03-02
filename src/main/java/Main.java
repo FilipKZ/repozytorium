7567a77989187094b9e4c82d34b5f161d3ebc7c0
Cineksikora96 <48150243+Cineksikora96@users.noreply.github.com>
2019-03-02 14:15:04 +0100

    komentarz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("coś innego niż tamten");
         for (int i = 0; i < 599; i++) {
+<<<<<<< Updated upstream
             System.out.println("ja dopisuję jako osoba 1:" + i);
             System.out.println("zmiana nowa");
         }
