a7492ddfc5ab004a485c7440a071ed43b3dc9f26
Cineksikora96 <48150243+Cineksikora96@users.noreply.github.com>
2019-03-02 13:41:12 +0100

    komentarz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("coś innego niż tamten");
         for (int i = 0; i < 100; i++) {
             System.out.println(i);
         }
