bd6ca5e5987be4a5b5670712cf580139f33cf202
Cineksikora96 <48150243+Cineksikora96@users.noreply.github.com>
2019-03-02 14:01:05 +0100

    komentarz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("coś innego niż tamten");
         for (int i = 0; i < 599; i++) {
-            System.out.println("dopisuje jako osoba 1:" + i);
+            System.out.println("ja dopisuję jako osoba 1:" + i);
         }
         System.out.println("Cośtam");
     }
